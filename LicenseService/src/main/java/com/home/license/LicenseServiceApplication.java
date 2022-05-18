3185f5584cb219628be4415682321d6c494f2282
crisr-mac <cristian.monterrosa@gmail.com>
2022-05-18 14:14:55 -0600

    Initial commit
    LicenseController
    License Model
    LicenseService
    
    messages.properties

---
+++ LicenseService/src/main/java/com/home/license/LicenseServiceApplication.java
@@ -0,0 +1,39 @@
+package com.home.license;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@SpringBootApplication
+public class LicenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseServiceApplication.class, args);
+	}
+	
+	// for Internationalization
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(true);
+		
+		//if we were in Italy we would have a file called messages_it.properties
+		messageSource.setBasenames("messages");
+		
+		return messageSource;
+	}
+	//End of Internationalization
+
+}
---
+++ LicenseService/src/test/java/com/home/license/LicenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
